2bb343dd981e6689706f89eed7077f1e2cf2fffb
frankblackjr <35541653+frankblackjr@users.noreply.github.com>
2018-10-10 22:46:48 -0400

    init

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $("button").click(function(){
+    $("img").attr("scr", "https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/28168796_1514512765310546_5986399144770518082_n.jpg?_nc_cat=108&oh=438bb10950860fec233382b36839820a&oe=5C48F4C2");
+  });
+});
